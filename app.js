0792ac2a4ce6ee49bb549c5d3c3991d8b3c3d285
sankar <sankaralingam@augustasoftsol.com>
2019-07-02 12:44:38 +0530

    update api added

--- app.js
+++ app.js
@@ -5,60 +5,103 @@ const fs = require('fs');
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 
 app.get('/:id', (req, res) => {
     console.log(req.params)
     fs.exists('data.json', (exists) => {
         if (exists) {
             fs.readFile('data.json', (err, data) => {
-                if (err){
+                if (err) {
                     console.log(err);
-                    res.status(400).send('Cannot able to read a file');
+                    res.send('Cannot able to read a file');
                 } else {
-                    const dataSet = JSON.parse(data); 
+                    const dataSet = JSON.parse(data);
                     res.send(dataSet[req.params.id]);
                 }
             });
         } else {
-            res.status(400).send('No data found.');
+            res.send('No data found.');
         }
     });
 });
 
 app.post('/', (req, res) => {
-    // console.log(req.body);
+    console.log(req.body);
     fs.exists('data.json', (exists) => {
         if (exists) {
             fs.readFile('data.json', (err, data) => {
-                if (err){
+                if (err) {
                     console.log(err);
-                    res.status(400).send('Cannot able to read a file');
+                    res.send('Cannot able to read a file');
                 } else {
-                    const dataSet = JSON.parse(data); 
-                    dataSet[req.body.id] = req.body;
-                    const json = JSON.stringify(dataSet); 
+                    const dataSet = JSON.parse(data);
+                    if (dataSet[req.body.id]) {
+                        dataSet[req.body.id].data = req.body;
+                    } else {
+                        dataSet[req.body.id] = {
+                            data: req.body,
+                            status: 0
+                        };
+                    }
+                    const json = JSON.stringify(dataSet);
                     fs.writeFile('data.json', json, (err) => {
                         if (err) {
                             console.log(err);
-                            res.status(400).send('Cannot able to creat a file');
+                            res.send('Cannot able to creat a file');
                         }
-                        res.send('Successfully added.');
+                        res.send(dataSet[req.body.id].status.toString());
                     });
                 }
             });
         } else {
-            // console.log(req.body);
+            console.log(req.body);
             const data = {};
-            data[req.body.id] = req.body;
+            data[req.body.id] = {
+                data: req.body,
+                status: 0
+            };
             const json = JSON.stringify(data);
             fs.writeFile('data.json', json, (err) => {
                 if (err) {
                     console.log(err);
-                    res.status(400).send('Cannot able to creat a file');
+                    res.send('Cannot able to creat a file');
                 }
-                res.send('Successfully added.');
+                res.send('0');
             });
+        }
+    });
+});
+
+app.put('/:id', (req, res) => {
+    console.log();
+    fs.exists('data.json', (exists) => {
+        if (exists) {
+            fs.readFile('data.json', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.send('Cannot able to read a file');
+                } else {
+                    const dataSet = JSON.parse(data);
+                    if (dataSet[req.params.id]) {
+                        dataSet[req.params.id].status = req.body.status;
+                        const json = JSON.stringify(dataSet);
+                        fs.writeFile('data.json', json, (err) => {
+                            if (err) {
+                                console.log(err);
+                                res.send('Cannot able to update data');
+                            }
+                            res.send(dataSet[req.params.id]);
+                        });
+                    } else {
+                        res.send('No data found.');
+                    }
+                }
+            });
+        } else {
+            res.send('No data found.');
         }
     });
 });
