5f0b887a64e7be8df8687987c7995006e9d307c2
iMac Augusta <>
2019-07-02 18:49:58 +0530

    ui updates

--- public/webservice.js
+++ public/webservice.js
@@ -1,8 +1,9 @@
 
 
-function getMotorAPI(deviceID) {
+function getMotorAPI() {
     $.ajax({
-    url: "http://localhost:3000/" + deviceID,
+    url: "http://localhost:3000/" + document.getElementById('txt_deviceid').value 
+    ,
     contentType: "application/json",
         dataType: 'json',
         success: function(result){
@@ -25,3 +26,28 @@ function getMotorAPI(deviceID) {
  });
 }
 
+function motorStatusUpdateAPI() {
+    $.ajax({
+    url: "http://localhost:3000/" + document.getElementById('txt_deviceid').value 
+    ,
+    contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+            console.log(result);
+            $(document).ready(function () {
+                $('#powerStatuslbl').html('Power Status :' + result.status);
+                $('#lblAMS').html('AMS Value :' + result.data.Currentvalue);
+                $('#lblVOL').html('Voltage Value :' + result.data.Voltagevalue);
+                document.getElementById("amsMeter").setAttribute("value", result.data.Currentvalue);
+                document.getElementById("volMeter").setAttribute("value", result.data.Voltagevalue);
+           });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status == 500) {
+                alert('Internal error: ' + jqXHR.responseText);
+            } else {
+                alert('Unexpected error.');
+            }
+        }
+ });
+}
